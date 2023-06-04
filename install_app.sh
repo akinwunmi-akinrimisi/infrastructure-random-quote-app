626786d7c826bd723e57cf767ffe2bfe871a8417
akinwunmi-akinrimisi <akinolaakinrimisi@gmail.com>
2023-06-04 05:13:27 +0100

    updated user data

--- install_app.sh
+++ install_app.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Install Apache web server
-sudo yum update -y
-sudo yum install -y httpd
+# Update the system and install Apache web server
+yum update -y
+yum install -y httpd
 
 # Start Apache web server
-sudo systemctl start httpd.service
+systemctl start httpd.service
 
-# Copy files from GitHub repository
-sudo yum install -y git
-git clone https://github.com/akinwunmi-akinrimisi/infrastructure-random-quote-app/tree/main/app /var/www/html/
+# Install Git
+yum install -y git
+
+# Clone the GitHub repository
+git clone https://github.com/akinwunmi-akinrimisi/infrastructure-random-quote-app.git /var/www/html/
 
 # Change ownership of the files to Apache user
-sudo chown -R apache:apache /var/www/html/
+chown -R apache:apache /var/www/html/
 
 # Restart Apache web server
-sudo systemctl restart httpd.service
-
-# sudo echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+systemctl restart httpd.service
