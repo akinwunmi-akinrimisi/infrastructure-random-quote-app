ce6d425c4211f8b50a6065b2f8af7bf750372601
akinwunmi-akinrimisi <akinolaakinrimisi@gmail.com>
2023-06-04 05:53:01 +0100

    updated the install_app.sh file to include file location changes

--- install_app.sh
+++ install_app.sh
@@ -13,6 +13,14 @@ yum install -y git
 # Clone the GitHub repository
 sudo git clone https://github.com/akinwunmi-akinrimisi/infrastructure-random-quote-app.git /var/www/html/
 
+#change directory to remove files not needed
+cd /var/www/html/
+sudo rm -f variables.tf install_app.sh Jenkinsfile main.tf
+
+#copy webfiles into the html directory
+mv /var/www/html/app/* .
+sudo rmdir  app/
+
 # Change ownership of the files to Apache user
 chown -R apache:apache /var/www/html/
 
