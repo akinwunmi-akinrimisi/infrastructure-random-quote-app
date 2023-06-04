f2ffdbbb506d8cc49434701d8f35a3cc9be8e595
akinwunmi-akinrimisi <akinolaakinrimisi@gmail.com>
2023-06-04 06:04:25 +0100

    updated the website styling

--- app/script.js
+++ app/script.js
@@ -1,27 +1,83 @@
-// Array of quotes
+
 const quotes = [
-    "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
-    "The way to get started is to quit talking and begin doing. - Walt Disney",
-    "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. - James Cameron",
-    "It does not matter how slowly you go as long as you do not stop. - Confucius",
-    "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
-    "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
-    "The best time to plant a tree was 20 years ago. The second best time is now. - Chinese Proverb",
-    "Believe you can and you're halfway there. - Theodore Roosevelt",
-    "I can't change the direction of the wind, but I can adjust my sails to always reach my destination. - Jimmy Dean",
-    "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt"
-];
+    "The best way to predict the future is to invent it.",
+    "The only way to do great work is to love what you do.",
+    "The only true wisdom is in knowing you know nothing.",
+    "You miss 100% of the shots you don’t take.",
+    "Believe you can and you’re halfway there.",
+    "It does not matter how slowly you go as long as you do not stop.",
+    "Success is not final, failure is not fatal, it’s the courage to continue that counts.",
+    "If you want to achieve greatness stop asking for permission.",
+    "The only limit to our realization of tomorrow will be our doubts of today.",
+    "If you don’t build your dream, someone else will hire you to help them build theirs."
+  ];
 
-// Function to generate a random quote
-function generateQuote() {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    const quoteText = document.getElementById("quoteText");
-    quoteText.textContent = quotes[randomIndex];
-}
+  const colors = [
+    "#16a085",
+    "#27ae60",
+    "#2c3e50",
+    "#f39c12",
+    "#e74c3c",
+    "#9b59b6",
+    "#FB6964",
+    "#342224",
+    "#472E32",
+    "#BDBB99",
+    "#77B1A9",
+    "#73A857"
+  ];
 
-// Event listener for the button click
-const generateBtn = document.getElementById("generateBtn");
-generateBtn.addEventListener("click", generateQuote);
+  function getRandomQuote() {
+      return quotes[Math.floor(Math.random() * quotes.length)];
+  }
 
-// Initial quote generation
-generateQuote();
+  function getRandomColor() {
+      return colors[Math.floor(Math.random() * colors.length)];
+  }
+
+  function setRandomQuote() {
+      const quote = document.getElementById("quote");
+      const generateButton = document.getElementById("generate");
+      const randomQuote = getRandomQuote();
+      const randomColor = getRandomColor();
+
+      document.body.style.backgroundColor = randomColor;
+
+      generateButton.style.backgroundColor = randomColor;
+
+      generateButton.style.color = '#fff';
+
+      document.body.style.backgroundImage = `url('https://source.unsplash.com/random')`;
+
+      document.body.style.transition = 'background-image 1s ease-in-out';
+
+      document.body.style.backgroundSize = 'cover';
+
+      document.body.style.backgroundRepeat = 'no-repeat';
+
+      document.body.style.backgroundPosition = 'center center';
+
+      generateButton.style.border = `2px solid ${randomColor}`;
+
+      generateButton.onmouseover = function() {
+          this.style.backgroundColor = '#fff';
+          this.style.color = randomColor;
+          this.style.transition = 'background-color 0.5s ease-in-out';
+          this.style.border = `2px solid ${randomColor}`;
+      };
+
+      generateButton.onmouseout = function() {
+          this.style.backgroundColor = randomColor;
+          this.style.color = '#fff';
+          this.style.transition = 'background-color 0.5s ease-in-out';
+          this.style.border = `2px solid ${randomColor}`;
+      };
+
+      generateButton.onclick = function() {
+          setRandomQuote();
+      };
+
+      return quote.innerHTML = randomQuote; 
+  }
+
+  setRandomQuote();
